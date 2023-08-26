89cdfb16c429a25732546faaec4a269a14490f55
Yilmaz Baris <123358369+ylmzbrscan@users.noreply.github.com>
2023-08-26 16:17:20 +0300

    done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
-function App() {
+export default function App() {
+  const [advice, setAdvice] = useState("0 advices");
+  const [count, setCount] = useState(0);
+
+  async function getAdvice() {
+    const res = await fetch("https://api.adviceslip.com/advice");
+    const data = await res.json();
+    setAdvice(data.slip.advice);
+    setCount((c) => c + 1);
+  }
+
+  useEffect(function () {
+    if (count > 0) {
+      getAdvice();
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>{advice}</h1>
+      <button onClick={getAdvice}>Get advice</button>
+      <Message count={count} />
     </div>
   );
 }
 
-export default App;
+function Message(props) {
+  return (
+    <p>
+      You have read <strong>{props.count}</strong> piece
+      {props.count !== 1 ? "s" : ""} of advice
+    </p>
+  );
+}
